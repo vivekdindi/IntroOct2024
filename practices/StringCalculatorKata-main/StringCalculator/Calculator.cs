1b29cf2e42f9d8e8e043fcf2776152b80fd2f15a
vivekdindi <vivek_dindigala@progressive.com>
2024-10-16 19:04:36 -0400

    Test 5 Mix Delimeters in progress

---
+++ practices/StringCalculatorKata-main/StringCalculator/Calculator.cs
@@ -0,0 +1,71 @@
+﻿public class Calculator
+{
+    public int Add(string numbers)
+    {
+        bool hasDelimiters = false;
+
+
+        // List<string> delimiters = new() { ",", "\n" };
+
+        if (numbers == "" || numbers.Length == 0)
+        {
+            return 0;
+        }
+
+        else if (numbers.Contains(","))
+        {
+
+            var sum = 0;
+            for (int i = 0; i < numbers.Split(',').Length; i++)
+            {
+                string num = numbers.Split(',')[i];
+                sum += Convert.ToInt32(num);
+
+            }
+            return sum;
+
+
+        }
+
+
+
+        else if (numbers.Contains("\n"))
+        {
+
+            var sum = 0;
+            for (int i = 0; i < numbers.Split('\n').Length; i++)
+            {
+                string num = numbers.Split('\n')[i];
+                sum += Convert.ToInt32(num);
+
+            }
+            return sum;
+
+        }
+
+        else if (numbers.Contains(",") || numbers.Contains("\n"))
+        {
+            var sum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (Char.IsDigit(numbers[i]))
+                {
+                    sum += Convert.ToInt32(numbers[i]);
+                }
+
+            }
+            return sum;
+
+        }
+
+
+
+        else return Convert.ToInt32(numbers);
+
+
+
+    }
+
+
+}
---
+++ practices/StringCalculatorKata-main/StringCalculator/CalculatorTests.cs
@@ -0,0 +1,89 @@
+﻿
+
+namespace StringCalculator;
+public class CalculatorTests
+{
+    [Fact]
+    public void EmptyStringReturnsZero()
+    {
+        var calculator = new Calculator();
+
+        var result = calculator.Add("");
+
+        Assert.Equal(0, result);
+    }
+
+    [Theory]
+    [InlineData("1", 1)]
+    [InlineData("-2", -2)]
+    public void singleInteger(string strNumber, int number)
+    {
+        var calculator = new Calculator();
+
+        var result = calculator.Add(strNumber);
+
+        Assert.Equal(result, number);
+    }
+
+    [Theory]
+    [InlineData("2,2", 4)]
+    [InlineData("-12,-2", -14)]
+    [InlineData("-10,2", -8)]
+
+
+    public void commaDelimiterTwoIntegers(string strNumber, int number)
+    {
+        var calculator = new Calculator();
+
+        var result = calculator.Add(strNumber);
+
+        Assert.Equal(result, number);
+    }
+
+
+
+    [Theory]
+    [InlineData("2,2", 4)]
+    [InlineData("1,3,3", 7)]
+    [InlineData("1,2,3,4,5,6,7,8,9", 45)]
+
+    public void commaDelimiterAnyLength(string strNumber, int number)
+    {
+        var calculator = new Calculator();
+
+        var result = calculator.Add(strNumber);
+
+        Assert.Equal(result, number);
+    }
+
+
+    //dd("1\n2") => 3`
+    [Theory]
+    [InlineData("2\n2", 4)]
+    [InlineData("2\n3\n4\n1\n5", 15)]
+
+    public void newLineDelimiter(string strNumber, int number)
+    {
+        var calculator = new Calculator();
+
+        var result = calculator.Add(strNumber);
+
+        Assert.Equal(result, number);
+    }
+
+    [Theory]
+    [InlineData("1\n,2", 3)]
+    [InlineData("2\n,3\n,4", 9)]
+    // `Add("1\n2,3") => 6`
+
+    public void mixDelimiter(string strNumber, int number)
+    {
+        var calculator = new Calculator();
+
+        var result = calculator.Add(strNumber);
+
+        Assert.Equal(result, number);
+    }
+
+
+}
