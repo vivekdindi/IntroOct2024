94d35923cce182db73ea3aed3b17a9b549f5bdd1
vivekdindi <vivek_dindigala@progressive.com>
2024-10-16 12:33:59 -0400

    making deposits

--- week1/BankingSolution/Banking.Domain/BankAccount.cs
+++ week1/BankingSolution/Banking.Domain/BankAccount.cs
@@ -1,14 +1,19 @@
 ﻿
+
 namespace Banking.Domain;
 
 public class BankAccount
 {
-    public BankAccount()
+    private decimal defaultBonus = 500M;
+
+    public void Deposit(decimal amountToDeposit)
     {
+        defaultBonus += amountToDeposit;
     }
 
     public decimal GetBalance()
     {
-        return 5000;
+
+        return defaultBonus;
     }
 }
---
+++ week1/BankingSolution/Banking.Tests/Account/MakingDeposits.cs
@@ -0,0 +1,23 @@
+﻿using Banking.Domain;
+
+namespace Banking.Tests.Account;
+public class MakingDeposits
+{
+    [Fact]
+    public void MakingADepositIncreasesOurBalance()
+    {
+        //given
+        var account = new BankAccount();
+        var openingBalance = account.GetBalance();
+        var amountToDeposit = 112.25M;
+
+        //when
+        account.Deposit(amountToDeposit);
+
+        //then
+        var endingBalance = account.GetBalance();
+        Assert.Equal(amountToDeposit + openingBalance, endingBalance);
+
+
+    }
+}
